cb74edfdba37d99cfd7b76585cd7b312cdc8601f
Fnac <Fnac@192.168.1.2>
2019-05-06 16:45:41 +0200

    Inserts corregidos e ids automáticos

--- web/js/bdFilenook.sql
+++ web/js/bdFilenook.sql
@@ -1,11 +1,3 @@
-
-
-
-
-
-
-
-
 DROP TABLE ClasificacionCategorias;
 DROP TABLE ValoracionComentario;
 DROP TABLE Comentario;
@@ -27,8 +19,7 @@ CREATE TABLE Usuario(
 
 
 CREATE TABLE Mensaje(
-	--idMensaje INTEGER GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
-        idMensaje INTEGER,
+	idMensaje INTEGER GENERATED ALWAYS AS IDENTITY (START WITH 0, INCREMENT BY 1),
 	texto VARCHAR(180) NOT NULL,
 	fecha DATE,
 	leido INTEGER CHECK (leido IN (0,1)),
@@ -44,8 +35,7 @@ CREATE TABLE Mensaje(
 
 
 CREATE TABLE Nook(
-	--idNook INTEGER GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
-        idNook INTEGER,
+	idNook INTEGER GENERATED ALWAYS AS IDENTITY (START WITH 0, INCREMENT BY 1),
 	nombre VARCHAR(50),
 	resumen VARCHAR(180),
 	autor VARCHAR(10),
@@ -84,7 +74,7 @@ CREATE TABLE ValoracionesNook(
 	usuario VARCHAR(10),
 	puntuacion INTEGER,
 	fecha DATE,
-	PRIMARY KEY (nook, usuario, fecha),
+	PRIMARY KEY (nook, usuario),
 	CONSTRAINT FK_VALNOOK FOREIGN KEY (nook)
 		REFERENCES Nook (idNook),
 	CONSTRAINT FK_VALNOOK2 FOREIGN KEY (usuario)
@@ -93,8 +83,7 @@ CREATE TABLE ValoracionesNook(
 
 
 CREATE TABLE Comentario(
-	--idComentario INTEGER GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
-        idComentario INTEGER,
+	idComentario INTEGER GENERATED ALWAYS AS IDENTITY (START WITH 0, INCREMENT BY 1),
 	fecha DATE,
 	autor VARCHAR(10),
 	texto VARCHAR(180),
@@ -109,7 +98,7 @@ CREATE TABLE ValoracionComentario(
 	puntuacion INTEGER,
 	fecha DATE,
 	usuario VARCHAR(10),
-	PRIMARY KEY (comentario, fecha, usuario),
+	PRIMARY KEY (comentario, usuario),
 	CONSTRAINT FK_VALCOMENTARIO FOREIGN KEY (comentario)
 		REFERENCES Comentario (idComentario),
 	CONSTRAINT FK_VALCOMENTARIO2 FOREIGN KEY (usuario)
@@ -118,8 +107,7 @@ CREATE TABLE ValoracionComentario(
 
 
 CREATE TABLE ClasificacionCategorias(
-	--idNook INTEGER GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
-        idNook INTEGER,
+	idNook INTEGER,
 	categoria VARCHAR(30),
 	PRIMARY KEY (idNook, categoria),
 	CONSTRAINT FK_CLASCATEGORIAS FOREIGN KEY (idNook)
@@ -134,11 +122,11 @@ INSERT INTO Usuario VALUES ('Rafael','Higelmo San Millán','rafhige','3344','and
 INSERT INTO Usuario VALUES ('Patricia','Aguado Labrador','patagua','5566','andres.cabero@alumnos.uva.es');
 INSERT INTO Usuario VALUES ('Santiago','Blasco Arnaiz','sanblas','7788','andres.cabero@alumnos.uva.es');
 
-INSERT INTO Mensaje VALUES (0,'Me gustaria que me explicase la función de la lía 127 de su código, gracias.','2019-04-09',1,'Enviado','patagua','rafhige');
-INSERT INTO Mensaje VALUES (1,'Me gustaria que me explicase la función de la lía 127 de su código, gracias.','2019-04-09',0,'Recibido','rafhige','patagua');
+INSERT INTO Mensaje (texto,fecha,leido,tipo,autor,destinatario) VALUES ('Me gustaria que me explicase la función de la lía 127 de su código, gracias.','2019-04-09',1,'Enviado','patagua','rafhige');
+INSERT INTO Mensaje (texto,fecha,leido,tipo,autor,destinatario) VALUES ('Me gustaria que me explicase la función de la lía 127 de su código, gracias.','2019-04-09',0,'Recibido','rafhige','patagua');
 
-INSERT INTO Nook VALUES (0,'MIPS','Prácticas en lenguaje ensamblador de primero de carrera de Ingeniería Informática','rafhige','2016-06-20','2016-06-21',25);
-INSERT INTO Nook VALUES (1,'Códigos Reed-Solomon','Ejercicios de la asignatura de Criptografía','patagua','2018-12-20','2018-12-20',10);
+INSERT INTO Nook (nombre,resumen,autor,fechaCreacion,fechaModificacion,descargas) VALUES ('MIPS','Prácticas en lenguaje ensamblador de primero de carrera de Ingeniería Informática','rafhige','2016-06-20','2016-06-21',0);
+INSERT INTO Nook (nombre,resumen,autor,fechaCreacion,fechaModificacion,descargas) VALUES ('Códigos Reed-Solomon','Ejercicios de la asignatura de Criptografía','patagua','2018-12-20','2018-12-20',0);
 
 INSERT INTO Documento VALUES (0,'Sumador.s','Programa MIPS para suamr número en hexadecimal y que muestre el resultado en binario complemento a dos.','2016-06-20','2016-06-20');
 INSERT INTO Documento VALUES (0,'Shunting.s','Algoritmo shunting yard implementado en MIPS mediante el uso de notación polaca inversa que devuelve resultados en decimal o hexadecimal como su entrada.','2016-06-20','2016-06-20');
@@ -151,13 +139,14 @@ INSERT INTO ValoracionesNook VALUES (0,'patagua',5,'2016-11-12');
 INSERT INTO ValoracionesNook VALUES (0,'sanblas',3,'2017-08-06');
 INSERT INTO ValoracionesNook VALUES (0,'andcabe',4,'2018-05-15');
 
-INSERT INTO Comentario VALUES (0,'2017-08-06','sanblas','Buen nook!!');
-INSERT INTO Comentario VALUES (1,'2016-11-13','andcabe','Tengo una duda con el archivo Sumador.s te mando un correo.');
+INSERT INTO Comentario (fecha,autor,texto) VALUES ('2017-08-06','sanblas','Buen nook!!');
+INSERT INTO Comentario (fecha,autor,texto) VALUES ('2016-11-13','andcabe','Tengo una duda con el archivo Sumador.s te mando un correo.');
 
 INSERT INTO ValoracionComentario VALUES (0,4,'2017-09-16','rafhige');
 
 INSERT INTO ClasificacionCategorias VALUES (0,'MIPS');
 INSERT INTO ClasificacionCategorias VALUES (0,'IngenieríaInformática');
+INSERT INTO ClasificacionCategorias VALUES (1,'IngenieríaInformática');
 INSERT INTO ClasificacionCategorias VALUES (0,'PrimerCurso');
 INSERT INTO ClasificacionCategorias VALUES (1,'Códigos');
 INSERT INTO ClasificacionCategorias VALUES (1,'Criptografía');
